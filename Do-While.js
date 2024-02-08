d4b6e112ab5088f1c1f061555b2c6677a17414b8
dmt061 <158508858+dmt061@users.noreply.github.com>
2024-02-08 14:29:43 -0500

    Add files via upload

---
+++ Do-While.js
@@ -0,0 +1,15 @@
+
+let refill = 3;
+
+
+do{
+    alert("Rellenar vaso" + refill);
+
+    if(refill ==1){
+        alert("No te quedan mas intentos")
+    }
+
+
+    refill--;
+}while(refill > 0 );
+
