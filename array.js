2de793a98d4ad14f3087c96e1a20c213011289ae
dmt061 <158508858+dmt061@users.noreply.github.com>
2024-02-12 14:31:13 -0500

    Add files via upload

--- array.js
+++ array.js
@@ -1,2 +1,29 @@
 
-let pokemon
+
+
+let pokemons=["pikachu", "firulais", "pigeoto", "El michu"];
+
+console.log(pokemons[0])
+console.log(pokemons[1])
+console.log(pokemons[2])
+console.log(pokemons[3])
+pokemons[4]= "charmkander";
+console.log(pokemons[4])
+
+for(let i = 0; i < pokemons.length; i++){
+    console.log("El nomnre de los pokemones es " + pokemons[i])
+}
+
+
+let ages = new Array();
+
+ages[0]=30
+ages[1]=20
+ages[2]=11
+ages[3]=29
+ages[4]=30
+
+for(let i=0; i < ages.length; i++){
+    console.log("Las edad numero" + i +"Es:" + ages[i])
+}
+
